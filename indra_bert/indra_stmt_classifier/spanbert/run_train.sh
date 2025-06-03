f5e5ad7e7839a65f06e7d17954112e7d5fc19456
Hyun Seung Lim <thomaslim6793@gmail.com>
2025-06-03 15:27:47 -0400

    Statement classifier for SpanBERT.

---
+++ indra_bert/indra_stmt_classifier/spanbert/run_train.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source ~/.bashrc
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate indra_gpt
+
+cd ~/gyorilab/indra_bert
+
+python -m indra_bert.indra_stmt_classifier.spanbert.train \
+    --dataset data/indra_benchmark_annotated_data/indra_benchmark_corpus_annotated_stratified_sample.jsonl \
+    --model_name SpanBERT/spanbert-base-cased \
+    --output_dir output/indra_stmt_classifier/spanbert/ \
