ed440fc6d89355d2fc5c11cd51569617bf55dec6
Hyun Seung Lim <thomaslim6793@gmail.com>
2025-06-11 16:42:36 -0400

    update run_train.sh

--- indra_bert/indra_stmt_classifier/run_train.sh
+++ indra_bert/indra_stmt_classifier/run_train.sh
@@ -7,7 +7,7 @@ conda activate indra_gpt
 cd ~/gyorilab/indra_bert
 
 python -m indra_bert.indra_stmt_classifier.train \
-    --dataset data/indra_benchmark_annotated_data/indra_benchmark_corpus_annotated_stratified_sample.jsonl \
+    --dataset data/indra_benchmark_annotated_data/indra_benchmark_corpus_annotated_stratified_sample_2000.jsonl \
     --output_dir output/indra_stmt_classifier \
     --model_name microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract \
     --epochs 8 \
